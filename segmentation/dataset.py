b8ae76f54c39edf3193370b8b9d9128dee0f4306
pavlos_aplakidis <aplakidisp@gmail.com>
2024-04-22 11:43:36 +0300

    pytorch dataset class for transformed data

--- segmentation/dataset.py
+++ segmentation/dataset.py
@@ -1,58 +1,56 @@
 #!/usr/bin/env python3
 import os
-import cv2
+import  matplotlib.pyplot as plt
+import random
 import numpy as np
-from medpy.io import load, save
 
-import torch
 from torch.utils.data import Dataset
 
 from utils import *
 
 class SegDataset(Dataset):
-  def __init__(self, base_dir, classes):
+  def __init__(self, base_dir):
     self.base_dir = base_dir
-    self.classes = classes
+    print("[+] Dataset base dir:", self.base_dir)
 
-    self.hgg_dir = base_dir + "HGG/"  # TODO: LGG data as well
-    self.patients = [self.hgg_dir + path for path in os.listdir(self.hgg_dir)]
+    images_path = os.path.join(self.base_dir, "images")
+    self.images = sorted(os.listdir(images_path))
+    for i in range(len(self.images)):
+      self.images[i] = os.path.join(images_path, self.images[i])
+
+    masks_path = os.path.join(self.base_dir, "masks")
+    self.masks = sorted(os.listdir(masks_path))
+    for i in range(len(self.masks)):
+      self.masks[i] = os.path.join(masks_path, self.masks[i])
+
+    assert len(self.images) == len(self.masks)
 
   def __len__(self):
-    return len(self.patients)
+    return len(self.images)
 
   def __getitem__(self, idx):
-    path = self.patients[idx]
-    files = os.listdir(path)
-    # TODO: flair is the image, seg is the mask
-    original = load(os.path.join(path, files[2]))[0]
-    seg = load(os.path.join(path, files[1]))[0]
-
-    mn=np.mean(original)
-    std=np.std(original)
-    pos=round((min(np.where(seg==1)[-1])+max(np.where(seg==1)[-1]))/2)
-    dev=np.std(list(range(original.shape[-1])))*0.08
-    
-    img=cv2.resize(original[:,:,pos+int(dev)], (224,224), interpolation = cv2.INTER_AREA )
-    img=(img-mn)/std
-    img=np.repeat(np.expand_dims(img,-1),3,-1)
-    
-    img=cv2.resize(original[:,:,pos-int(dev)], (224,224), interpolation = cv2.INTER_AREA )
-    img=(img-mn)/std
-    img=np.repeat(np.expand_dims(img,-1),3,-1)
-    
-    
-    img=cv2.resize(original[:,:,pos], (224,224), interpolation = cv2.INTER_AREA )
-    img=(img-mn)/std
-    img=np.repeat(np.expand_dims(img,-1),3,-1)
-
-    print(img.shape)
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    return img
+    images = np.load(self.images[idx])
+    masks = np.load(self.masks[idx])
+    return {"images": images, "masks": masks}
 
 
 if __name__ == "__main__":
-  dataset = SegDataset(BASE_DIR_SEG, [])
-  data = dataset[0]
+  dataset = SegDataset(BASE_DIR_SEG)
+  sample = dataset[0]
+
+  images, masks = sample["images"], sample["masks"]
+  print(images.shape, masks.shape)
+
+  n_slice = random.randint(0, masks.shape[2]-1)
+  masks_view = from_categorical(masks)
+
+  plt.figure(figsize=(12, 8))
+
+  plt.subplot(221)
+  plt.imshow(images[:, :, n_slice, 0], cmap='gray')
+  plt.title('Image')
+
+  plt.subplot(222)
+  plt.imshow(masks_view[:, :, n_slice])
+  plt.title('Mask')
+  plt.show()
--- segmentation/utils.py
+++ segmentation/utils.py
@@ -1,2 +1,12 @@
+import numpy as np
+
 W = H = 224
-BASE_DIR_SEG = '../data/MICCAI_BraTS_2019_Data_Training/'
+BASE_DIR_SEG = "../data/BraTS2020/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/input_data_3channels"
+
+# 1-hot encodes a tensor
+def to_categorical(y, num_classes):
+  return np.eye(num_classes, dtype='uint8')[y]
+
+# TODO: test this (might be ignoring classes)
+def from_categorical(encoded_array):
+  return np.argmax(encoded_array, axis=-1)
