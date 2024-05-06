d1802181cc97c7ed30820913a0ba5e700a833248
pavlos_aplakidis <aplakidisp@gmail.com>
2024-05-06 21:38:48 +0300

    classification dataset class

---
+++ classification/dataset.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import os
+import  matplotlib.pyplot as plt
+import random
+import numpy as np
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+from utils import *
+
+
+class ClassificationDataset(Dataset):
+  def __init__(self, base_dir):
+    self.base_dir = base_dir
+
+    self.label_names = sorted(os.listdir(base_dir))
+    print(self.label_names)
+    self.image_paths = []
+    self.labels = []
+    for idx, dirr in enumerate(sorted(os.listdir(base_dir))):
+      for image_path in sorted(os.listdir(os.path.join(base_dir, dirr))):
+        self.image_paths.append(os.path.join(base_dir, dirr, image_path))
+        self.labels.append(idx)
+
+    assert len(self.image_paths) == len(self.labels)
+
+    self.transform = transforms.Compose([
+      transforms.Resize((W, H)),
+      transforms.ToTensor(),
+    ])
+  
+  def __len__(self):
+    return len(self.image_paths)
+
+  def __getitem__(self, idx):
+    img = self.transform(Image.open(self.image_paths[idx])).unsqueeze(0).float()
+    return {"image": img, "label": self.labels[idx]}
+
+
+if __name__ == "__main__":
+  dataset = ClassificationDataset(BASE_DIR_TRAIN)
+  print(len(dataset))
+  data = dataset[0]
+  image, label = data["image"], data["label"]
+  print(label, "-", image)
+  print(image.shape)
---
+++ classification/utils.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+W = H = 128
+
+BASE_DIR_TRAIN = "../data/Brain_Tumor_MRI_Dataset/Training"
+BASE_DIR_TEST = "../data/Brain_Tumor_MRI_Dataset/Testing"
+
+TB_PATH = "runs/" + str(datetime.now())
