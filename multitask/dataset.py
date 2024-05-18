d2e73626ae4c59fb16ae870302bb530f48f64e71
pavlos_aplakidis <aplakidisp@gmail.com>
2024-05-18 18:48:56 +0300

    multitask dataset getitem: return label as well

--- multitask/dataset.py
+++ multitask/dataset.py
@@ -19,8 +19,6 @@ class MultitaskDataset(Dataset):
     self.classes= sorted(os.listdir(BASE_DIR_CLF))
     with open(self.classes_path) as f:
       self.clf_labels = json.load(f)
-    print(self.classes)
-    print(self.clf_labels)
 
     images_path = os.path.join(self.base_dir, "images")
     self.images = sorted(os.listdir(images_path))
@@ -38,16 +36,18 @@ class MultitaskDataset(Dataset):
   def __len__(self):
     return len(self.images)
 
-  # TODO: return class as well
   def __getitem__(self, idx):
+    image_name = self.images[idx].split('/')[-1]
+    print(image_name)
     image = np.load(self.images[idx])
     image = np.moveaxis(image, -1, 0)
 
     if not self.test:
       mask = np.load(self.masks[idx])
       mask = np.moveaxis(mask, -1, 0)
+      label = self.clf_labels[image_name]
 
-      return {"image": image, "mask": mask}
+      return {"image": image, "mask": mask, "label": label}
     else:
       return {"image": image}
 
@@ -57,8 +57,8 @@ if __name__ == "__main__":
   print(len(dataset))
   sample = dataset[0]
 
-  image, mask = sample["image"], sample["mask"]
-  print(image.shape, mask.shape)
+  image, mask, label = sample["image"], sample["mask"], sample["label"]
+  print(image.shape, mask.shape, label)
   image = np.moveaxis(image, 0, -1)
   mask = np.moveaxis(mask, 0, -1)
 
@@ -74,5 +74,9 @@ if __name__ == "__main__":
   plt.subplot(222)
   plt.imshow(mask_view)
   plt.title('Mask')
+
+  plt.subplot(223)
+  plt.title(dataset.classes[label])
+
   plt.show()
 
